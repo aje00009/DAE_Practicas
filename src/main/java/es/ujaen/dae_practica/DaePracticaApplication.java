faf97546878b8d4ff2315a37426ebc75a26cdc90
Alberto Jiménez Expósito <aje00009@red.ujaen.es>
2025-09-29 20:40:16 +0200

    Inicialización proyecto

---
+++ src/main/java/es/ujaen/dae_practica/DaePracticaApplication.java
@@ -0,0 +1,13 @@
+package es.ujaen.dae_practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaePracticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaePracticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/ujaen/dae_practica/DaePracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ujaen.dae_practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaePracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
